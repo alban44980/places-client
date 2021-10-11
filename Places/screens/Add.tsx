e910612999fd742d399d0a241c5cc3dda925536d
Alban Mansord <alban.mansord@gmail.com>
2021-10-11 18:16:49 +0200

    basic navigation

---
+++ Places/screens/Add.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ListViewBase,
+} from 'react-native';
+
+function Add() {
+  return <Text>Add Screen!!!</Text>;
+}
+
+export default Add;
---
+++ Places/screens/Friends.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ListViewBase,
+} from 'react-native';
+
+function Friends() {
+  return <Text>Friends screen!!!</Text>;
+}
+
+export default Friends;
