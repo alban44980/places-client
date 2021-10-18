06b3d5b86e12545674c2fdf686575d97b7cc1f73
Will Sukonik <will.sukonik@icloud.com>
2021-10-18 14:57:30 +0200

    tab bar custom in progress

--- Places/components/Navigation/Navigation.tsx
+++ Places/components/Navigation/Navigation.tsx
@@ -15,25 +15,36 @@ import Friends from '../../screens/Friends';
 import Add from '../../screens/Add';
 import Profile from '../../screens/Profile';
 import UserProfile from '../UserProfile/UserProfile';
+import fonts from '../../assets/styles/fonts';
+import colors from '../../assets/styles/colors';
+
 
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator 
+      screenOptions={{ 
+        headerShown: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarActiveTintColor: colors.fontLight,
+        tabBarInactiveTintColor: colors.fontDark,
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarActiveBackgroundColor: colors.backgroundDark,
+        tabBarItemStyle: styles.tabBarItem
+       }} 
+       
+    >
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ focused }) => (
-            <View>
+              tabBarIcon: ({ focused }) => (
+            <View style={styles.tabContainer}>
               <Image
                 source={require('../../assets/navigation/home.png')}
                 resizeMode="contain"
-                style={{
-                  width: 30,
-                  height: 30,
-                }}
+                style={styles.iconStyle}
               />
             </View>
           ),
@@ -44,14 +55,11 @@ function Tabs() {
         component={Friends}
         options={{
           tabBarIcon: ({ focused }) => (
-            <View>
+            <View style={styles.tabContainer}>
               <Image
                 source={require('../../assets/navigation/friends.png')}
                 resizeMode="contain"
-                style={{
-                  width: 30,
-                  height: 30,
-                }}
+                style={styles.iconStyle}
               />
             </View>
           ),
@@ -62,14 +70,11 @@ function Tabs() {
         component={Add}
         options={{
           tabBarIcon: ({ focused }) => (
-            <View>
+            <View style={styles.tabContainer}>
               <Image
                 source={require('../../assets/navigation/plus.png')}
                 resizeMode="contain"
-                style={{
-                  width: 30,
-                  height: 30,
-                }}
+                style={styles.iconStyle}
               />
             </View>
           ),
@@ -80,14 +85,11 @@ function Tabs() {
         component={Profile}
         options={{
           tabBarIcon: ({ focused }) => (
-            <View>
+            <View style={styles.tabContainer}>
               <Image
                 source={require('../../assets/navigation/robot.png')}
                 resizeMode="contain"
-                style={{
-                  width: 30,
-                  height: 30,
-                }}
+                style={styles.iconStyle}
               />
             </View>
           ),
@@ -97,6 +99,49 @@ function Tabs() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: colors.backgroundMedium, 
+    height: 60, 
+    width: '90%',
+    bottom: 20, 
+    borderRadius: 20, 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    borderWidth: 2, 
+    borderStyle: 'solid',
+  },
+
+  tabBarItem: {
+    borderWidth: 1, 
+    borderStyle: 'solid', 
+    flex: 1,    
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+
+  tabContainer: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    height: 30,
+    width: 30,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginBottom: 10,
+  },
+
+  tabBarLabelStyle: {
+    fontFamily: fonts.light,
+    letterSpacing: .5,
+    marginTop: 2,
+    flex: 1,
+  },
+
+
+  iconStyle: {
+    height: 24,
+    width: 24,
+  }
+});
 
 export default Tabs;
