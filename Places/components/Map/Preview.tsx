a044b1ec30217923f2b08af7ad27b383673e7124
Will Sukonik <will.sukonik@icloud.com>
2021-10-20 21:35:43 +0200

    map complete

--- Places/components/Map/Preview.tsx
+++ Places/components/Map/Preview.tsx
@@ -3,13 +3,19 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import colors from '../../assets/styles/colors';
 import { BlurView } from 'expo-blur';
+import fonts from '../../assets/styles/fonts';
 
 function Preview({ currentPlaceReview, setPlaceVisible }: any) {
   return (
     <BlurView intensity={100} style={styles.placePreviewContainer}>
       <View style={styles.imageContainer}>
         <Image
-          style={{ width: '80%', height: '80%', borderRadius: 20 }}
+          style={{
+            width: '80%',
+            height: '80%',
+            borderRadius: 20,
+            borderWidth: 1,
+          }}
           source={{
             uri: currentPlaceReview.img,
           }}
@@ -21,11 +27,7 @@ function Preview({ currentPlaceReview, setPlaceVisible }: any) {
         </View>
         <View style={styles.tagsContainer}>
           {currentPlaceReview.Tags.map((tag: any) => {
-            return (
-              <Text style={{ color: colors.fontLight, margin: 2 }}>
-                #{tag.name}
-              </Text>
-            );
+            return <Text style={styles.tagsStyle}>#{tag.name}</Text>;
           })}
         </View>
         <View style={styles.buttonContainer}>
@@ -35,7 +37,7 @@ function Preview({ currentPlaceReview, setPlaceVisible }: any) {
               setPlaceVisible(true);
             }}
           >
-            <Text style={{ fontSize: 20 }}>View</Text>
+            <Text style={{ fontSize: 15, fontFamily: fonts.medium }}>View</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -50,24 +52,45 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 230,
     zIndex: 2,
+    width: '100%',
+    justifyContent: 'space-between',
   },
   imageContainer: {
     width: '50%',
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   infoContainer: {
-    width: '50%',
+    width: '45%',
+    backgroundColor: colors.backgroundLight,
+    right: 15,
+    top: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    borderRadius: 10,
+    height: '80%',
+    borderWidth: 0.25,
   },
+
   placeName: {
     height: '30%',
     justifyContent: 'center',
-    paddingLeft: 60,
   },
+
   title: {
-    color: colors.fontLight,
+    color: colors.fontDark,
     fontSize: 18,
+    fontFamily: fonts.medium,
   },
+
+  tagsStyle: {
+    color: colors.fontDark,
+    margin: 2,
+    fontFamily: fonts.medium,
+    fontSize: 11,
+  },
+
   tagsContainer: {
     height: '25%',
     flexDirection: 'row',
@@ -75,18 +98,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   buttonContainer: {
-    flex: 1,
+    height: '25%',
     justifyContent: 'center',
-    marginTop: 10,
+    marginTop: 20,
+    width: '100%',
+    alignItems: 'center',
+    alignSelf: 'flex-end',
   },
+
   viewButton: {
     height: '70%',
-    width: '70%',
-    backgroundColor: colors.backgroundBright,
+    width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 20,
-    marginLeft: 30,
+    borderRadius: 5,
+    fontSize: 12,
   },
 });
 
