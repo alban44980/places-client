887461a76f7ae134659087cb28e51c4a21cb9036
Juliana Halko <julianayasnog@gmail.com>
2021-10-20 16:05:13 +0200

    styles and colors adjustments

--- Places/assets/styles/colors.js
+++ Places/assets/styles/colors.js
@@ -7,9 +7,9 @@ const colors = {
   backgroundMedium: 'rgba(174, 217, 224, 1)',
   backgroundLight: 'rgba(250, 243, 221, 1)',
   backgroundBright: 'rgba(250, 243, 221, 1)',
-  fontDark: 'rgba(32, 30, 31, 1)',
+  fontDark: 'rgba(128, 128, 128, 1)',
   fontLight: 'rgba(250, 243, 221, 1)',
-  accentFun: 'rgba(184, 242, 230, 1)',
+  accentFun: 'rgba(102, 178, 255, 1)',
   formInputBackgroundLight: 'rgba(255, 255, 255, 1)',
 };
 
