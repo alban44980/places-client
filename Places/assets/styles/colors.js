21f97cd766c6d6c263778db2314d7c59d80136d7
Will Sukonik <will.sukonik@icloud.com>
2021-10-21 09:05:32 +0200

    small font color change

--- Places/assets/styles/colors.js
+++ Places/assets/styles/colors.js
@@ -3,7 +3,7 @@ const colors = {
   backgroundMedium: 'rgba(128, 128, 128, 1)',
   backgroundLight: 'rgba(250, 255, 255, 1)',
   backgroundBright: 'rgba(250, 243, 221, 1)',
-  fontDark: 'rgba(94, 100, 114, 1)',
+  fontDark: 'rgba(28, 28, 28, 1)',
   fontLight: 'rgba(250, 243, 221, 1)',
   accentFun: 'rgba(102, 178, 255, 1)',
   formInputBackgroundLight: 'rgba(255, 255, 255, 1)',
